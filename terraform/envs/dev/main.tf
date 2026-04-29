907bec8214b4ec11ae3e76eba2abcda9cafd08f4
saketVishwakarma <svsaket123@gmail.com>
2026-04-29 13:51:23 +0530

    stage done

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -20,3 +20,44 @@ module "vnet" {
     app_subnet = "10.0.2.0/24"
   }
 }
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "devacr${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "dev-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "devaks"
+
+  default_node_pool {
+    name       = "nodepool"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+}
